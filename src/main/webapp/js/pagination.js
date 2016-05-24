4b8e7ffe66d6109bbf26617521055214b959b606
zncanke <ckzheng0320@outlook.com>
2016-05-24 21:47:47 +0800

    next page pre page ok

--- src/main/webapp/js/pagination.js
+++ src/main/webapp/js/pagination.js
@@ -1,8 +1,8 @@
 /**
  * 分页页面内容获取，该函数只能注册到分页的a标签上，ajax成功后调用回调方法callback
  */
-function clickGoto(callback, $this) {
-    var url = $this.attr("url");
+function clickGoto(callback, a) {
+    var url = a.attr("url");
     $.ajax({
         type: "GET",
         url: url,
@@ -18,8 +18,8 @@ function clickGoto(callback, $this) {
  * 分页页面内容获取，该函数只能注册到分页的input.text标签上,ajax成功后调用回调方法callback
  */
 function changeGoto(callback) {
-    var url = $this.attr("url");
-    var page = $this.val();
+    var url = a.attr("url");
+    var page = a.val();
     url = url + "&page="+page;
     $.ajax({
         type: "GET",
